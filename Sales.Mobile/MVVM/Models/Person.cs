b3710ab8bfab3063b75fc74a428b4cd4f7211756
Jarangos <94191961+Jarangos31@users.noreply.github.com>
2023-05-14 18:08:06 -0500

    MVVM2

--- Sales.Mobile/MVVM/Models/Person.cs
+++ Sales.Mobile/MVVM/Models/Person.cs
@@ -11,5 +11,13 @@ namespace Sales.Mobile.MVVM.Models
         public String Name { get; set; }
 
         public int Age { get; set; }
+
+        public bool IsMarried { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public int Weight { get; set; }
+
+        public TimeSpan LunchTime { get; set; }
     }
 }
